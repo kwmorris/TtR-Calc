3f6442cd2bf6f630302e30210da31993e5682804
kwmorris <k.w.morris@gmail.com>
2015-05-13 13:25:29 -0700

    Removed placeholder Scoreboard text Added layout for card list

--- app/src/main/java/com/labs/odyn/ttrcalc/ScoreboardFragment.java
+++ app/src/main/java/com/labs/odyn/ttrcalc/ScoreboardFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 
 public class ScoreboardFragment extends Fragment {
@@ -27,9 +26,6 @@ public class ScoreboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scoreboard, container, false);
-        TextView placeHolder = (TextView) view.findViewById(R.id.placeHolderSB);
-        placeHolder.setText("Place Holder Scoreboard");
         return view;
-
     }
 }
