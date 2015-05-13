0483c8331cf00d8f2caffef51b69ce3f62777b2a
kwmorris <k.w.morris@gmail.com>
2015-05-13 13:13:10 -0700

    populated Global Variables

--- app/src/main/java/com/labs/odyn/ttrcalc/Globals.java
+++ app/src/main/java/com/labs/odyn/ttrcalc/Globals.java
@@ -13,6 +13,44 @@ public class Globals {
         return instance;
     }
 
+    //Set and Get playerNames
+    private static String[] playerNames = new String[5];
 
+    public void setPlayerNames(String[] pNames){
+        playerNames = pNames;
+    }
 
+    public void setPlayerNames(String pName, int pos){
+        playerNames[pos] = pName;
+    }
+
+    public String[] getPlayerNames(){
+        return playerNames;
+    }
+
+    public String getPlayerNames(int pos){
+        return playerNames[pos];
+    }
+
+    //Set and Get playerScores
+    private static int[] playerScores = new int[5];
+
+    public void setPlayerScores(int[] pScores){
+        playerScores = pScores;
+    }
+
+    public void setPlayerScores(int pScore, int pos){
+        playerScores[pos] = pScore;
+    }
+
+    public int[] getPlayerScores(){
+        return playerScores;
+    }
+
+    public int getPlayerScores(int pos){
+        return playerScores[pos];
+    }
+
+    //Set and Get playerColor
+    private String[] playerColor = new String[5];
 }
