be227efe3e0167c2a570da5565af1cbe8f031bcc
kwmorris <k.w.morris@gmail.com>
2015-05-13 11:07:22 -0700

    Implementing the Tab Strip - added Scoreboard Fragment (cleaned)

--- app/src/main/java/com/labs/odyn/ttrcalc/ScoreboardFragment.java
+++ app/src/main/java/com/labs/odyn/ttrcalc/ScoreboardFragment.java
@@ -12,8 +12,6 @@ import android.widget.TextView;
 
 public class ScoreboardFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
-
     public static ScoreboardFragment newInstance() {
         return new ScoreboardFragment();
     }
@@ -36,44 +34,4 @@ public class ScoreboardFragment extends Fragment {
         return view;
 
     }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
-
 }
