46a38608e626d09b1bbbeabe76dd585d035b818a
kwmorris <k.w.morris@gmail.com>
2015-05-14 15:29:19 -0700

    Added EditCard class

---
+++ app/src/main/java/com/labs/odyn/ttrcalc/EditCard.java
@@ -0,0 +1,53 @@
+package com.labs.odyn.ttrcalc;
+
+import android.content.Context;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import it.gmariotti.cardslib.library.internal.Card;
+
+public class EditCard extends Card {
+
+    protected String pName;
+
+    //Globals
+    Globals g = new Globals().getInstance();
+
+    public EditCard(Context context, int player){
+        this(context);
+        pName = g.getPlayerNames(player);;
+    }
+
+    public EditCard(Context context) {
+        super(context, R.layout.edit_card);
+    }
+
+
+
+    @Override
+    public void setupInnerViewElements(ViewGroup parent, View view){
+        //Retrieve elements
+        EditText editName = (EditText) view.findViewById(R.id.editTitle);
+
+        editName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE){
+
+                }
+                return handled;
+            }
+        });
+
+        editName.setHint(pName);
+    }
+
+    public void setName (int player){
+        pName = g.getPlayerNames(player);
+    }
+}
--- app/src/main/java/com/labs/odyn/ttrcalc/EditFragment.java
+++ app/src/main/java/com/labs/odyn/ttrcalc/EditFragment.java
@@ -7,8 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.view.CardViewNative;
+
 
 public class EditFragment extends Fragment {
+
+    private static Globals g = new Globals().getInstance();
+
+    private static EditCard cardEdit1;
+    private static EditCard cardEdit2;
+    private static EditCard cardEdit3;
+    private static EditCard cardEdit4;
+    private static EditCard cardEdit5;
 
     public static EditFragment newInstance() {
         return new EditFragment();
@@ -24,12 +35,66 @@ public class EditFragment extends Fragment {
     }*/
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit, container, false);
-        TextView placeHolder = (TextView) view.findViewById(R.id.placeHolderE);
-        placeHolder.setText("Place Holder Edit");
-        return view;
 
+        cardEdit1 = createCard(0);
+        cardEdit2 = createCard(1);
+        cardEdit3 = createCard(2);
+        cardEdit4 = createCard(3);
+        cardEdit5 = createCard(4);
+
+        cardEdit1.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+            }
+        });
+
+        cardEdit2.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+            }
+        });
+
+        cardEdit3.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+            }
+        });
+
+        cardEdit4.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+            }
+        });
+
+        cardEdit5.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+            }
+        });
+
+
+        CardViewNative edit1View = (CardViewNative) view.findViewById(R.id.cardEdit1);
+        CardViewNative edit2View = (CardViewNative) view.findViewById(R.id.cardEdit2);
+        CardViewNative edit3View = (CardViewNative) view.findViewById(R.id.cardEdit3);
+        CardViewNative edit4View = (CardViewNative) view.findViewById(R.id.cardEdit4);
+        CardViewNative edit5View = (CardViewNative) view.findViewById(R.id.cardEdit5);
+
+        edit1View.setCard(cardEdit1);
+        edit2View.setCard(cardEdit2);
+        edit3View.setCard(cardEdit3);
+        edit4View.setCard(cardEdit4);
+        edit5View.setCard(cardEdit5);
+
+        return view;
+    }
+
+    private EditCard createCard(int player){
+        EditCard card = new EditCard(this.getActivity(), player);
+
+        card.setBackgroundColorResourceId(g.getColorPrimary(g.getPlayerColors(player)));
+
+        return card;
     }
 }
